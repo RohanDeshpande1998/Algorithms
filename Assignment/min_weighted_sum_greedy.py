e582b9028b07cab25d80fda61a1eed8fa44de2e1
Rohan <rohandeshpande55@gmail.com>
2021-03-26 20:39:33 +0530

    Correct Code

--- Assignment/min_weighted_sum_greedy.py
+++ Assignment/min_weighted_sum_greedy.py
@@ -1,29 +1,17 @@
-from operator import itemgetter
-
-def greedy_min_weighted_sum(arr):
-    length = 0
-    weight = 0
-    weighted_answer = []
-    temp = []
-    new_list = [[el[0],el[1],el[0]/el[1]] for el in arr[1:]]     #change the way to schedule job(subtraction or division[currently division])
-    final_list = []
-    new_list.sort(key=itemgetter(2), reverse = True)
-    first_el = new_list[0][2]
-    for el in new_list:
-        if el[2] == first_el:
-            temp.append(el)
-        else:
-            final_list.extend(sorted(temp,key = itemgetter(0),reverse = True))
-            temp = []
-        first_el = el[2]
-    for el in final_list:
-        weight = el[0]
-        length = length + el[1]
-        weighted_answer.append(weight*length)
+def greedy_min_weighted_sum(jobs):
+    finish_time = 0
+    weighted_sum = 0
+    priority = [(item[0]/item[1], item[0]) for item in jobs]
+    order = sorted(range(len(priority)), key = priority.__getitem__)
+    order.reverse()
+    for i in order:
         
-    return(sum(weighted_answer))
+        finish_time += jobs[i][1]
+        weighted_sum += finish_time*jobs[i][0]
+        
+    return weighted_sum
 
 
 with open("Assignment/Programming_Assignment_3_1_1.txt") as assignment:
     array = [list(map(int, line.split())) for line in assignment]
-    print(greedy_min_weighted_sum(array))
+    print(greedy_min_weighted_sum(array[1:]))
